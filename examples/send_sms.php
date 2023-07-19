c35b047b6565cfecba653be59a68a5a11c7c7839
aimedidierm <aimedidiermugisha@gmail.com>
2023-07-19 18:46:11 +0200

    Removing guzzlehttp

--- examples/send_sms.php
+++ examples/send_sms.php
@@ -1,15 +1,17 @@
 <?php
 
-namespace Aimedidierm\FdiSms;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$sms = new SendSms;
+use Aimedidierm\FdiSms\SendSms;
+
+$sms = new SendSms();
 $sms->SingleSms(
     $username = "", // Your API Username provided by FDI
     $password = "", // Your API Password provided by FDI
     $sender_id = "", // Your User ID provided by FDI
     $phone = "", // Receiver phone number
-    $message = "", // Text message to be send
+    $message = "", // Text message to be sent
     $ref = "", // Your unique message reference ID
-    $callBackURL = "" //Optional Delivery Report destination
+    $callBackURL = "" // Optional Delivery Report destination
 );
-echo $sms;
+var_dump($sms);
--- src/SendSms.php
+++ src/SendSms.php
@@ -2,10 +2,19 @@
 
 namespace Aimedidierm\FdiSms;
 
+use Exception;
+
 class SendSms
 {
     private function authFDISMS($SMSSECRET, $SMSID)
     {
+        if (empty($SMSSECRET)) {
+            throw new Exception("SMSSECRET is required.");
+        }
+        if (empty($SMSID)) {
+            throw new Exception("SMSID is required.");
+        }
+
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, "https://messaging.fdibiz.com/api/v1/auth/");
@@ -14,10 +23,10 @@ class SendSms
 
         curl_setopt($ch, CURLOPT_POST, TRUE);
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "{
-        \"api_username\": $SMSID,
-        \"api_password\": $SMSSECRET
-        }");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+            "api_username" => $SMSID,
+            "api_password" => $SMSSECRET
+        ]));
 
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             "Content-Type: application/json",
@@ -26,14 +35,31 @@ class SendSms
 
         $response = curl_exec($ch);
         curl_close($ch);
-
-        var_dump($response);
+        $decodedResponse = json_decode($response, true);
+        return $decodedResponse;
     }
 
     public function SingleSms($username, $password, $sender_id, $phone, $message, $ref, $callBackURL)
     {
+        if (empty($username)) {
+            throw new Exception("Username is required.");
+        }
+        if (empty($password)) {
+            throw new Exception("Password is required.");
+        }
+        if (empty($phone)) {
+            throw new Exception("Phone number is required.");
+        }
+        if (empty($message)) {
+            throw new Exception("Message is required.");
+        }
+        if (empty($ref)) {
+            throw new Exception("Reference is required.");
+        }
+
         $auth = $this->authFDISMS($password, $username);
-        $token = $auth['access_token'];
+        $token = $auth["access_token"];
+        var_dump($token);
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, "https://messaging.fdibiz.com/api/v1/mt/single");
@@ -42,13 +68,13 @@ class SendSms
 
         curl_setopt($ch, CURLOPT_POST, TRUE);
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "{
-        \"msisdn\": $phone,
-        \"message\": $message,
-        \"msgRef\": $ref,
-        \"dlr\": $callBackURL,
-        \"sender_id\": $sender_id
-        }");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+            "msisdn" => $phone,
+            "message" => $message,
+            "msgRef" => $ref,
+            "dlr" => $callBackURL,
+            "sender_id" => $sender_id
+        ]));
 
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
             "Content-Type: application/json",
